d0aebf88ce16ff09a860d7bfa33f974eafbd9622
Vlad Zarytovskii <vlza@microsoft.com>
2017-03-20 17:59:21 +0100

    WIP: Add some verification & validation to the http context. Cleaned up some unnecesary stuff, which is already in System.Net.Http

--- Fs.Helpers/Fs.Helpers.Http.fs
+++ Fs.Helpers/Fs.Helpers.Http.fs
@@ -2,33 +2,23 @@ namespace Fs.Helpers.Http
 
 module HttpExtensions =
     open System
-    open System.Net
+    open System.Net.Http
     open System.IO
 
     open FSharp.Core
     open FSharp.Reflection
-
-    [<Literal>] let private CRLF = "\r\n"
-    // From http://www.fssnip.net/9l/title/toString-and-fromString-for-discriminated-unions
-    let private unionToString (x: 'a) =
-        match FSharpValue.GetUnionFields(x, typeof<'a>) with
-        | case, _ -> case.Name
-        | _ -> "UNKNOWN"
 
     let private tryParseUri (uri: string) : Uri option =
         match Uri.TryCreate(uri, UriKind.Absolute) with
         | (true, url) -> Some url
         | _ -> None
 
-    type HttpMethod = GET | POST | PUT | DELETE | TRACE | PATCH | CONNECT | HEAD | OPTIONS
-        with override this.ToString() = unionToString this
-
     type HttpHeader = string * string
 
     type HttpContext = {
         method: HttpMethod option;
         uri: Uri option;
-        headers: HttpHeader list option;
+        headers: (HttpHeader list) option;
         body: byte array option;
         timeout: int option;
         keepalive: bool option;
@@ -42,9 +32,22 @@ module HttpExtensions =
         body: Stream option
     }
 
+    // TODO:
+    // Not sure that would be the proper way of "verifying" the context
+    // Maybe it's worth it to do some more "monadic" way
     let private validateHttpContext(ctx: HttpContext) : HttpContext option =
-        None
-    let private createHttpRequest (ctx: HttpContext) : HttpResponse option =
+        match ctx with
+        | ctx when ctx.method = None || ctx.uri = None  -> None
+        | _ -> Some ctx
+
+    let private performHttpRequest (ctx: HttpContext) : HttpResponse option =
+        use client = new HttpClient ()
+        use message = new HttpRequestMessage ()
+        message.Method <- Option.get ctx.method
+        message.RequestUri <- Option.get ctx.uri
+        match ctx.headers with
+        | Some headers -> headers |> Seq.iter message.Headers.Add
+        | None -> ()
         None
 
     type HttpBuilder () =
@@ -61,25 +64,23 @@ module HttpExtensions =
         [<CustomOperation("timeout", MaintainsVariableSpace=true)>]
         member this.Timeout(ctx: HttpContext, timeout: int) =
             { ctx with timeout = Some timeout}
-            (* HTTP related Methods *)
         [<CustomOperation("keepalive", MaintainsVariableSpace=true)>]
         member this.Keepalive(ctx: HttpContext, keepalive: bool) =
             { ctx with keepalive = Some keepalive}
         [<CustomOperation("GET", MaintainsVariableSpace=true)>]
-        member this.GET(ctx: HttpContext, uri: string, headers: (string * string) list) =
-            { ctx with method = Some GET; uri = uri |> tryParseUri; headers = Some headers }
+        member this.GET(ctx: HttpContext, uri: string, headers: (string * string) list option) =
+            { ctx with method = Some HttpMethod.Get; uri = uri |> tryParseUri; headers = headers }
 
         member __.Run(ctx: HttpContext) =
             match ctx |> validateHttpContext with
-            | Some ctx -> ctx |> createHttpRequest
+            | Some ctx -> ctx |> performHttpRequest
             | None -> None
 
     let http = HttpBuilder()
+
     let response = http {
             timeout 10_000  // 10 seconds
             keepalive false
-            GET "https://microsoft.com/" [
-                ("X-Custom-Header", "HeaderValue")
-            ]
-        }
+            GET "https://microsoft.com/" None
+    }
     // response |> printfn
